221284f22e8a086350e270c21ce1881546b2bfb8
shaohua yang <shaohua0110@yahoo.com>
2016-06-08 03:05:05 +0800

    get objc types

---
+++ Kaleidoscope/tool.cpp
@@ -0,0 +1,121 @@
+//
+//  tool.cpp
+//  Kaleidoscope
+//
+//  Created by shaohua on 6/7/16.
+//  Copyright © 2016 syang. All rights reserved.
+//
+
+#include <clang/AST/RecursiveASTVisitor.h>
+#include <clang/Frontend/FrontendActions.h>
+#include <clang/Tooling/Tooling.h>
+
+#include <fstream>
+#include <iostream>
+#include <cassert>
+
+class FindNamedClassVisitor : public clang::RecursiveASTVisitor<FindNamedClassVisitor> {
+public:
+    explicit FindNamedClassVisitor(clang::ASTContext &Context)
+    : Context(&Context) {
+    }
+
+    bool VisitDecl(clang::Decl *Decl) {
+        std::string Name;
+        std::string Types;
+        switch (Decl->getKind()) {
+            case clang::Decl::Kind::Record:
+            case clang::Decl::Kind::Enum:
+            case clang::Decl::Kind::ObjCInterface:
+            case clang::Decl::Kind::ObjCProtocol:
+            case clang::Decl::Kind::ObjCCategory:
+            {
+                clang::NamedDecl *Named = clang::cast<clang::NamedDecl>(Decl);
+                Name = Named->getNameAsString();
+                if (Name.length() == 0) {
+                    return true;
+                }
+                std::cout << "<" << Name << ">\n";
+                return true;
+            }
+            case clang::Decl::Kind::EnumConstant: {
+                clang::EnumConstantDecl *Enum = clang::cast<clang::EnumConstantDecl>(Decl);
+                Context->getObjCEncodingForType(Enum->getType(), Types);
+                Name = Enum->getNameAsString();
+                break;
+            }
+            case clang::Decl::Kind::Var: {
+                clang::VarDecl *VD = clang::cast<clang::VarDecl>(Decl);
+                Context->getObjCEncodingForType(VD->getType(), Types);
+                Name = VD->getNameAsString();
+                break;
+            }
+            case clang::Decl::Kind::Function:{
+                clang::FunctionDecl *Function = clang::cast<clang::FunctionDecl>(Decl);
+                Context->getObjCEncodingForFunctionDecl(Function, Types);
+                Name = Function->getNameAsString();
+                break;
+            }
+            case clang::Decl::Kind::ObjCProperty: {
+                clang::ObjCPropertyDecl *Property = clang::cast<clang::ObjCPropertyDecl>(Decl);
+                Context->getObjCEncodingForPropertyDecl(Property, nullptr, Types);
+                Name = Property->getNameAsString();
+                break;
+            }
+            case clang::Decl::Kind::ObjCMethod: {
+                clang::ObjCMethodDecl *Method = clang::cast<clang::ObjCMethodDecl>(Decl);
+                clang::Selector Selector = Method->getSelector();
+                Context->getObjCEncodingForMethodDecl(Method, Types);
+                Name = Selector.getAsString();
+                break;
+            }
+            default:
+                return true;
+        }
+        assert(Name.length() <= 2 && "no name");
+        std::cout << "  -[" << Name << " " << Types << "]\n";
+        return true;
+    }
+private:
+    clang::ASTContext *Context;
+};
+
+class FindNamedClassConsumer : public clang::ASTConsumer {
+public:
+    virtual void HandleTranslationUnit(clang::ASTContext &Context) {
+        Visitor = llvm::make_unique<FindNamedClassVisitor>(Context);
+        Visitor->TraverseDecl(Context.getTranslationUnitDecl());
+    }
+private:
+    std::unique_ptr<FindNamedClassVisitor> Visitor;
+};
+
+class FindNamedClassAction : public clang::ASTFrontendAction {
+public:
+    virtual std::unique_ptr<clang::ASTConsumer> CreateASTConsumer(clang::CompilerInstance &Compiler, llvm::StringRef InFile) {
+        return std::unique_ptr<clang::ASTConsumer>(new FindNamedClassConsumer);
+    }
+};
+
+int main(int argc, const char **argv) {
+
+    std::string Sdk = "/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneSimulator.platform/Developer/SDKs/iPhoneSimulator.sdk";
+    std::string Arch = "x86_64"; // i386 x86_64 arm arm64
+    std::string Path = Sdk + "/System/Library/Frameworks/Foundation.framework/Headers/NSArray.h";
+
+    std::ifstream File(Path);
+    typedef std::istreambuf_iterator<char> Iterator;
+    std::istreambuf_iterator<char> It(File);
+    std::istreambuf_iterator<char> Eof;
+    std::string Code(Iterator(File), (Iterator()));
+
+    std::vector<std::string> Args = {
+        "-ObjC++", "-Wall", "-isysroot", Sdk, "-arch", Arch, "-miphoneos-version-min=7.0", "-std=c++11",
+        // http://llvm.org/releases/3.4/tools/clang/docs/LibTooling.html
+        // search "../lib/clang/3.8.0/include" by default
+        "-I/usr/local/Cellar/llvm/3.8.0/lib/clang/3.8.0/include",
+        // "-v",
+    };
+    clang::tooling::runToolOnCodeWithArgs(new FindNamedClassAction, Code, Args);
+    return 0;
+}
